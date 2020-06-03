1e8739191609c192320e3ad226e60b905ea889be
Palculator <37553150+Palculator@users.noreply.github.com>
2020-06-03 14:22:59 +0200

    Add missing license header

--- src/beamngpy/lua/researchCommunication.lua
+++ src/beamngpy/lua/researchCommunication.lua
@@ -1,3 +1,7 @@
+-- This Source Code Form is subject to the terms of the bCDDL, v. 1.1.
+-- If a copy of the bCDDL was not distributed with this
+-- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
+
 local M = {}
 
 local mp = require('libs/lua-MessagePack/MessagePack')
