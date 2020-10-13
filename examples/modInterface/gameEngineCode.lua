60baa9c6e1d2f7ea1363a6af9eb26b465b67cbe7
Pascale <pmaul@beamng.com>
2020-10-13 18:15:07 +0200

    new example: how to use a custom mod with python

---
+++ examples/modInterface/gameEngineCode.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+local rcom = require('utils/researchCommunication')
+
+local _log = log
+local function log(level, msg)
+  _log(level, 'gameEngineCode', msg)
+end
+
+local function handleFoo(skt, msg)
+    print("Hello " .. msg['someName'] .. '!\n')
+    rcom.sendACK(skt, 'Goodbye!')
+end
+
+local function onSocketMessage(skt, msg)
+    log("D", "onSocketMessage")
+    dump(msg)
+    local msgType = 'handle' .. msg['type']
+    local handler = M[msgType]
+    if handler ~= nil then
+        handler(skt, msg)
+    else
+        log("E", "handler does not exist: " .. msgType)
+    end
+end
+
+M.onSocketMessage = onSocketMessage
+M.handleFoo = handleFoo
+
+return M
