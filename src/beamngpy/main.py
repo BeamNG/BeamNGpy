724da3688f403c81e29683ae26333ecdb2c16e59
Adam Ivora <aivora@beamng.gmbh>
2022-03-14 08:38:39 +0100

    Make the CLI commands `setup-workspace` and `deploy-mod` deprecated.

--- src/beamngpy/main.py
+++ src/beamngpy/main.py
@@ -1,8 +1,7 @@
 import click
 from logging import getLogger, DEBUG
-from pathlib import Path
-from beamngpy import BeamNGpy
-from beamngpy.beamngcommon import LOGGER_ID
+from beamngpy.beamngcommon import LOGGER_ID, create_warning
+import warnings
 
 
 module_logger = getLogger(f'{LOGGER_ID}.main')
@@ -17,7 +16,10 @@ def cli():
 @cli.command()
 @click.argument('userpath', type=click.Path(file_okay=False))
 def deploy_mod(userpath):
-    BeamNGpy.deploy_mod(userpath)
+    # to show the deprecation warnings when running CLI commands
+    warnings.simplefilter('always', DeprecationWarning)
+    create_warning('`deploy-mod` command is not used and deprecated since BeamNG.tech version 0.25.',
+        DeprecationWarning)
 
 
 @cli.command()
@@ -25,24 +27,10 @@ def deploy_mod(userpath):
 @click.option('--host', '-h', default='localhost')
 @click.option('--port', '-p', default=64256)
 def setup_workspace(userpath, host, port):
-    licence = Path(userpath)
-    research_licence = licence / 'research.key'
-    tech_licence = licence / 'tech.key'
-    if not(research_licence.exists()) and not(tech_licence.exists()):
-        module_logger.error('Cannot set up workspace if no '
-                            f'licence key is available at <{userpath}>.')
-        return
-
-    bng = BeamNGpy(host, port, user=userpath)
-    bng.setup_workspace()
-
-    bng.determine_effective_userpath()
-    if bng.effective_user is not None:
-        module_logger.info(f'Set up workspace at <{userpath}>.')
-    else:
-        module_logger.error(f'Could not set up workspace at <{userpath}>.'
-                            'Note that this step is only neccessary for '
-                            'BeamNG.tech version 0.22 and above.')
+    # to show the deprecation warnings when running CLI commands
+    warnings.simplefilter('always', DeprecationWarning)
+    create_warning('`setup-workspace` command is not needed and deprecated since BeamNG.tech version 0.25.',
+        DeprecationWarning)
 
 
 if __name__ == '__main__':
