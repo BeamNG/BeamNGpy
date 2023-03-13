0132240e65b3cc4127d35d19f470d9a4effbcc66
Adam Ivora <aivora@beamng.gmbh>
2023-03-13 09:22:12 +0100

    Add MeshRoad to the `road_definition.py` example

--- examples/road_definition.py
+++ examples/road_definition.py
@@ -9,7 +9,7 @@
 
 """
 
-from beamngpy import BeamNGpy, Road, Scenario, Vehicle
+from beamngpy import BeamNGpy, MeshRoad, Road, Scenario, Vehicle
 
 
 def main():
@@ -20,6 +20,7 @@ def main():
     vehicle = Vehicle('ego_vehicle', model='etk800')
     scenario.add_vehicle(vehicle, pos=(-25, 300, 100), rot_quat=(0, 0, 0, 1))
 
+    # create a decal road
     road_a = Road('track_editor_C_center', rid='circle_road', looped=True)
     road_a.add_nodes(
         (-25, 300, 100, 5),
@@ -35,13 +36,26 @@ def main():
         (50, 375, 100, 5)
     )
     scenario.add_road(road_b)
+    
+    # create a mesh road
+    mesh_road = MeshRoad('track_editor_C_center', rid='main_road')
+    mesh_road.add_nodes(
+        (-107, 20, 100),
+        (-157, 20, 110),
+        (-207, 25, 120, 15, 10), # the 4th and 5th arguments are width and depth of the node
+        (-257, 15, 110),
+        (-307, 20, 100, 30), 
+    )
+    scenario.add_mesh_road(mesh_road)
 
     scenario.make(beamng)
 
     try:
         beamng.scenario.load(scenario)
         beamng.scenario.start()
-        input('Press enter when done...')
+        input('Press Enter to teleport to the other road...')
+        vehicle.teleport((-107, 20, 100), reset=False)
+        input('Press Enter when done...')
     finally:
         beamng.close()
 
