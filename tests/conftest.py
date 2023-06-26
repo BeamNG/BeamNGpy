1e425e30dde0e80cde01cb11a4093568b554c1e5
Adam Ivora <aivora@beamng.gmbh>
2023-06-26 14:15:55 +0200

    Add `crash_lua_on_error` option to the BeamNGpy constructor

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,4 +13,4 @@ def run_before_tests():
 
 @pytest.fixture(scope='session')
 def beamng() -> BeamNGpy:
-    return BeamNGpy('localhost', 64256, quit_on_close=False)
+    return BeamNGpy('localhost', 64256, quit_on_close=False, crash_lua_on_error=True)
