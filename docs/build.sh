5fcd7a30b5c392d5f661676d1e480d220b44ec0a
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-02-12 16:54:09 +0100

    Update docs build script

--- docs/build.sh
+++ docs/build.sh
@@ -8,8 +8,14 @@ cd "$SCRIPT_DIR"
 
 # Add relevant branches locally
 git fetch origin master:master
+if [ "$USE_DEV_BRANCH" == "true" ]; then
+    git fetch origin docs-multiversion:dev
+fi
 
 # Build docs
 pip install -r source/requirements.txt
 sphinx-multiversion source build
 cp root_template.html build/index.html
+if [ "$USE_DEV_BRANCH" == "true" ]; then
+    cp root_template_dev.html build/index.html
+fi
