fdb64b559c2c786639b09d2876eccd90ee637a8e
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-02-12 16:25:45 +0100

    Update script

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Build script for documentation pages
+set -eo pipefail
 
 # Go to this directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
 # Build docs
-python test.py
+pip install -r source/requirements.txt
+sphinx-multiversion source build
+cp root_template.html build/index.html
