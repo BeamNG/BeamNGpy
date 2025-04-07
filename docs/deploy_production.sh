c5fa7367a9095fc3b61040869d6be345dc40c7e7
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-04-07 13:35:28 +0200

    Cleanup script

--- docs/deploy_production.sh
+++ docs/deploy_production.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Deploy script for documentation pages
+# Will host built documentation on https://documentation.beamng.com/api/beamngpy
 set -eo pipefail
-
-# Variables
-echo $DOCUMENTATION_ARTIFACTS_PATH
-echo $DOCUMENTATION_TARGET_PATH
 
 # Set up SSH
 mkdir -p ~/.ssh
@@ -16,24 +13,21 @@ REPO_HOST=${REPO_HOST%:*}
 ssh-keyscan -t ed25519 $REPO_HOST >> ~/.ssh/known_hosts
 
 # Clone repository
-LOCAL_REPO_PATH="docs/production"
-mkdir -p $LOCAL_REPO_PATH
-git clone -b $DOCUMENTATION_BRANCH $DOCUMENTATION_REPOSITORY $LOCAL_REPO_PATH
+mkdir -p $DOCUMENTATION_LOCAL_REPO
+git clone -b $DOCUMENTATION_BRANCH $DOCUMENTATION_REPOSITORY $DOCUMENTATION_LOCAL_REPO
 
 # Git author config
 git config --global user.name "$DOCUMENTATION_GIT_USERNAME"
 git config --global user.email "$DOCUMENTATION_GIT_EMAIL"
 
 # Copy artifacts
-DEST_PATH="$PWD/$LOCAL_REPO_PATH/$DOCUMENTATION_TARGET_PATH"
+DEST_PATH="$PWD/$DOCUMENTATION_LOCAL_REPO/$DOCUMENTATION_TARGET_PATH"
 rm -rf "$DEST_PATH"
 mkdir -p "$(dirname "$DEST_PATH")"
 cp -r "$DOCUMENTATION_ARTIFACTS_PATH" "$DEST_PATH"
-ls -al "$DEST_PATH"
 
 # Commit and push
-cd "$LOCAL_REPO_PATH"
+cd "$DOCUMENTATION_LOCAL_REPO"
 git add --all
 git commit -m "Update $DOCUMENTATION_TARGET_PATH"
-git status
 git push
