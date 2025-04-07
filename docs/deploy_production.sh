080011872bee357ae3bc62c1caea614e057bb5d8
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-04-07 11:05:19 +0200

    Update deploy docs script

--- docs/deploy_production.sh
+++ docs/deploy_production.sh
@@ -18,7 +18,21 @@ ssh-keyscan -t ed25519 $REPO_HOST >> ~/.ssh/known_hosts
 # Clone repository
 LOCAL_REPO_PATH="docs/production"
 mkdir -p $LOCAL_REPO_PATH
-git clone $DOCUMENTATION_REPOSITORY $LOCAL_REPO_PATH
+git clone -b $DOCUMENTATION_BRANCH $DOCUMENTATION_REPOSITORY $LOCAL_REPO_PATH
 
-# TODO
-ls $LOCAL_REPO_PATH
+# Git author config
+git config --global user.name "$DOCUMENTATION_GIT_USERNAME"
+git config --global user.email "$DOCUMENTATION_GIT_EMAIL"
+
+# Copy artifacts
+DEST_PATH="$PWD/$LOCAL_REPO_PATH/$DOCUMENTATION_TARGET_PATH"
+rm -rf "$DEST_PATH"
+cp -r "$DOCUMENTATION_ARTIFACTS_PATH" "$DEST_PATH"
+ls -al "$DEST_PATH"
+
+# Commit and push
+cd "$LOCAL_REPO_PATH"
+git add --all
+git commit -m "Update $DOCUMENTATION_TARGET_PATH"
+git status
+#git push
