e05831b312c52bb64ae05195c9232af1d069e3b9
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-04-07 10:16:39 +0200

    Update deploy docs script

--- docs/deploy_production.sh
+++ docs/deploy_production.sh
@@ -10,3 +10,16 @@ echo $DOCUMENTATION_GIT_EMAIL
 echo $DOCUMENTATION_ARTIFACTS_PATH
 echo $DOCUMENTATION_TARGET_PATH
 
+# Set up SSH
+mkdir -p ~/.ssh
+echo $DOCUMENTATION_SSH_PRIVATE_KEY > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+ssh-keyscan -t ed25519 $DOCUMENTATION_REPOSITORY >> ~/.ssh/known_hosts
+
+# Clone repository
+LOCAL_REPO_PATH="docs/production"
+mkdir -p $LOCAL_REPO_PATH
+git clone $DOCUMENTATION_REPOSITORY $LOCAL_REPO_PATH
+
+# TODO
+ls $LOCAL_REPO_PATH
