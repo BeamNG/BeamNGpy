67262831a4bd1c27a03adae9b34f01f340cc9f74
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-04-07 10:56:01 +0200

    Fix script

--- docs/deploy_production.sh
+++ docs/deploy_production.sh
@@ -3,17 +3,14 @@
 set -eo pipefail
 
 # Variables
-echo $DOCUMENTATION_REPOSITORY
-echo $DOCUMENTATION_BRANCH
 echo $DOCUMENTATION_ARTIFACTS_PATH
 echo $DOCUMENTATION_TARGET_PATH
 
 # Set up SSH
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo "$DOCUMENTATION_SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
-chmod 400 ~/.ssh/id_ed25519
-ls -al ~/.ssh
+echo "$DOCUMENTATION_SSH_PRIVATE_KEY" | base64 --decode > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
 REPO_HOST=${DOCUMENTATION_REPOSITORY#*@}
 REPO_HOST=${REPO_HOST%:*}
 ssh-keyscan -t ed25519 $REPO_HOST >> ~/.ssh/known_hosts
