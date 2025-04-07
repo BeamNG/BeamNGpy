5e289e4a60c60ac5f0a0770c95610da80af9966f
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-04-07 10:46:55 +0200

    Add debug output

--- docs/deploy_production.sh
+++ docs/deploy_production.sh
@@ -5,15 +5,15 @@ set -eo pipefail
 # Variables
 echo $DOCUMENTATION_REPOSITORY
 echo $DOCUMENTATION_BRANCH
-echo $DOCUMENTATION_GIT_USERNAME
-echo $DOCUMENTATION_GIT_EMAIL
 echo $DOCUMENTATION_ARTIFACTS_PATH
 echo $DOCUMENTATION_TARGET_PATH
 
 # Set up SSH
 mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 echo "$DOCUMENTATION_SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
-chmod 600 ~/.ssh/id_ed25519
+chmod 400 ~/.ssh/id_ed25519
+ls -al ~/.ssh
 REPO_HOST=${DOCUMENTATION_REPOSITORY#*@}
 REPO_HOST=${REPO_HOST%:*}
 ssh-keyscan -t ed25519 $REPO_HOST >> ~/.ssh/known_hosts
