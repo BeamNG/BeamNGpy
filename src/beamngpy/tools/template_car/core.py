52d5c53118e733e44320e63e623c823c5c79112c
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-12-05 15:17:41 +0100

    Add docstrings

--- src/beamngpy/tools/template_car/core.py
+++ src/beamngpy/tools/template_car/core.py
@@ -12,10 +12,31 @@ from .models import Settings, TemplateVehicle
 
 
 class TemplateCarGenerator:
-    """Template car generator."""
+    """Template car generator.
+    
+    The template car generator is a tool that allows you to parameterize and generate template cars for BeamNG.
+    This tool comes with a graphical user interface that can be started with:
+    ```
+    python -m beamngpy.tools.template_car
+    ```
+    or by creating a desktop shortcut with:
+    ```
+    python -m beamngpy.tools.template_car --create-shortcut
+    ```
+    For more information about the graphical user interface, see the
+    [documentation](https://documentation.beamng.com/beamng_tech/tools/).
+
+    Use this class to programmatically generate template cars.
+
+    Args:
+        settings_file: Path to a settings JSON file. If not provided, the default settings file will be used.
+    
+    Returns:
+        TemplateCarGenerator: Template car generator instance.
+    """
 
     def __init__(self, settings_file: str|None=None):
-        """Initialize template car generator."""
+        """Initialize this class."""
         if settings_file:
             settings_file = os.path.abspath(settings_file)
         else:
@@ -24,21 +45,25 @@ class TemplateCarGenerator:
 
     @property
     def vehicles_folder(self) -> str:
-        """Return path to vehicles folder."""
+        """The expanded absolute path to vehicles folder."""
         return get_vehicles_folder(self.get_settings())
 
     @property
     def user_folder(self) -> str:
-        """Return path to BeamNG user folder."""
+        """The expanded absolute path to the BeamNG user folder."""
         return get_user_folder(self.get_settings())
-    
+
     @property
     def install_path(self) -> str:
         """Return path to BeamNG install folder."""
         return get_beamng_install_path(self.get_settings())
 
     def get_settings(self) -> Settings:
-        """Read settings from file."""
+        """Read settings from file.
+        
+        Returns:
+            settings: The settings object.
+        """
         if os.path.isfile(self.settings_file):
             settings = read_json(self.settings_file)
         else:
@@ -46,7 +71,11 @@ class TemplateCarGenerator:
         return Settings.model_validate(settings)
 
     def get_vehicle_list(self) -> list[str]:
-        """Return list of all vehicle IDs."""
+        """Return list of all vehicle IDs.
+        
+        Returns:
+            vehicle_list: List of all vehicle IDs (strings).
+        """
         vehicle_list = []
         for file in os.listdir(self.vehicles_folder):
             if file.endswith(".json"):
@@ -55,16 +84,30 @@ class TemplateCarGenerator:
         return vehicle_list
 
     def get_vehicle(self, vehicle_id: str) -> TemplateVehicle:
-        """Return TemplateVehicle object by ID."""
+        """Return TemplateVehicle object by ID.
+        
+        Returns:
+            vehicle: The TemplateVehicle object.
+        """
         v = read_json(get_vehicle_path(self.vehicles_folder, vehicle_id))
         return TemplateVehicle.model_validate(v)
 
     def generate(self, vehicle_id: str, vehicle: TemplateVehicle) -> None:
-        """Optimize and generate the vehicle mod and move it to BeamNG mods folder."""
+        """Optimize and generate the vehicle mod and move it to BeamNG mods folder.
+        
+        Args:
+            vehicle_id: The ID to be used for the generated mod vehicle.
+            vehicle: The TemplateVehicle object to generate.
+        """
         generate_optimal_car(vehicle_id, vehicle, self.user_folder, self.install_path)
 
     def play(self, vehicle_id: str, level_id: str) -> None:
-        """Launch BeamNG with the generated vehicle mod."""
+        """Launch BeamNG with the generated vehicle mod.
+        
+        Args:
+            vehicle_id: The ID of the vehicle to play.
+            level_id: The ID of the level to play.
+        """
         install_path = Path(self.install_path)
         args = ["-level", level_id, "-vehicle", vehicle_id, "-userpath", self.user_folder]
         binary = determine_binary(install_path)
